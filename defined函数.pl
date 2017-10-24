1e1d18eadcb6aa354e84005b6db506a425ecf82c
Ccccong <caiCongcong0322>
2017-10-24 14:46:19 +0800

    a

---
+++ defined函数.pl
@@ -0,0 +1,7 @@
+#$madonn=<stdin>;
+$madonn=undef;
+if(defined($madonn)){
+	print "the input was $madonn";
+}else{
+	print "No input available\n";
+}
--- test.pl
+++ test.pl
@@ -1,13 +1,4 @@
-print "hello world";
-chomp($test=<stdin>);
 
-print $test;
 
-print "<stdin>\n";
-$n=1;
-while($n<10){
-	$sum +=$n;
-	$n+=2;
-}
-
-print "The total was $sum.\n";
+$c+=2;
+print $c;
